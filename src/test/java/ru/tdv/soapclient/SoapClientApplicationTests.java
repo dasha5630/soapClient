af3037c648ee0e5c1d60ad59d8486a94abad805d
Daria <dasha5630@yandex.ru>
2020-04-18 18:28:09 +0300

    classes were generated with jaxb2

---
+++ src/test/java/ru/tdv/soapclient/SoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tdv.soapclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
